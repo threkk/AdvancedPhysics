d59abbca9d0a90db581e7fff72136faae10c996b
carmonj001 <harryperch@hotmail.com>
2014-03-06 12:12:07 +0100

    Update mobile.cpp
    casi terminao

--- Assignment 3/Assignment 3/mobile.cpp
+++ Assignment 3/Assignment 3/mobile.cpp
@@ -8,6 +8,7 @@
 
 #include <gl/glut.h>
 #include <cyclone/cyclone.h>
+#include <math.h>
 #include "../../app.h"
 #include "../../timing.h"
 
@@ -17,7 +18,7 @@
  * 10 particles: 1 fixed, 5 for the cross, 4 hanging in the springs.
  */
 #define PARTICLES_COUNT 10
-#define ROD_COUNT 6
+#define ROD_COUNT 10
 #define CABLE_COUNT 5
 #define SPRING_COUNT 4
 
@@ -29,7 +30,7 @@ class Mobile : public MassAggregateApplication
 {
     ParticleRod *rods;
     ParticleCableConstraint *cables;
-    ParticleAnchoredSpring *springs;
+    ParticleSpring *springs [SPRING_COUNT];
     
 public:
     Mobile();
@@ -44,7 +45,7 @@ public:
 
 // TODO: Crear la figura.
 // Constructor
-Mobile::Mobile() : MassAggregateApplication(PARTICLES_COUNT), cables(0), rods(0), springs(0){
+Mobile::Mobile() : MassAggregateApplication(PARTICLES_COUNT), cables(0), rods(0){
 
 
     // Fixed point, everthing hangs from here.
@@ -80,16 +81,16 @@ Mobile::Mobile() : MassAggregateApplication(PARTICLES_COUNT), cables(0), rods(0)
 			particleArray[i].setPosition(0.0, 4.0, -3.0);
 			break;
 		case 6 :
-			particleArray[i].setPosition(3.0, 0.0, 0.0);
+			particleArray[i].setPosition(3.0, 2.0, 0.0);
 			break;
 		case 7 :
-			particleArray[i].setPosition(-3.0, 0.0, 0.0);
+			particleArray[i].setPosition(-3.0, 2.0, 0.0);
 			break;
 		case 8 :
-			particleArray[i].setPosition(0.0, 0.0, 3.0);
+			particleArray[i].setPosition(0.0, 2.0, 3.0);
 			break;
 		case 9 :
-			particleArray[i].setPosition(0.0, 0.0, -3.0);
+			particleArray[i].setPosition(0.0, 2.0, -3.0);
 			break;
 
 		}
@@ -107,7 +108,7 @@ Mobile::Mobile() : MassAggregateApplication(PARTICLES_COUNT), cables(0), rods(0)
 	for (unsigned i = 0; i < 4; i++){
 		rods[i].particle[0] = &particleArray[1];
 		rods[i].particle[1] = &particleArray[i+2];
-		rods[i].length = 3;
+		rods[i].length = 3.0;
 		world.getContactGenerators().push_back(&rods[i]);
 	}
 
@@ -120,6 +121,26 @@ Mobile::Mobile() : MassAggregateApplication(PARTICLES_COUNT), cables(0), rods(0)
 	rods[5].particle[1] = &particleArray[5];
 	rods[5].length = 6;
 	world.getContactGenerators().push_back(&rods[5]);
+
+	rods[6].particle[0] = &particleArray[2];
+	rods[6].particle[1] = &particleArray[4];
+	rods[6].length = sqrt(18.0);
+	world.getContactGenerators().push_back(&rods[6]);
+
+	rods[7].particle[0] = &particleArray[2];
+	rods[7].particle[1] = &particleArray[5];
+	rods[7].length = sqrt(18.0);
+	world.getContactGenerators().push_back(&rods[7]);
+
+	rods[8].particle[0] = &particleArray[3];
+	rods[8].particle[1] = &particleArray[5];
+	rods[8].length = sqrt(18.0);
+	world.getContactGenerators().push_back(&rods[8]);
+
+	rods[9].particle[0] = &particleArray[3];
+	rods[9].particle[1] = &particleArray[4];
+	rods[9].length = sqrt(18.0);
+	world.getContactGenerators().push_back(&rods[9]);
 
 	
 	// Create the cables
@@ -141,8 +162,9 @@ Mobile::Mobile() : MassAggregateApplication(PARTICLES_COUNT), cables(0), rods(0)
 
 
 	// Create the springs
-	springs = new ParticleAnchoredSpring[SPRING_COUNT];
-
+	for(unsigned i=0; i<SPRING_COUNT; i++){
+		springs[i] = new ParticleSpring(&particleArray[i+6],90.0,0.5);
+	}
 	
     
 }
@@ -159,6 +181,10 @@ Mobile::~Mobile()
 void Mobile::update()
 {
     MassAggregateApplication::update();
+
+	for (unsigned i = 0; i < SPRING_COUNT; i++) {
+       springs[i]->updateForce(&particleArray[2+i], 1);
+    }
     
 }
 
@@ -200,15 +226,19 @@ void Mobile::display()
     }
     
 	
-	/*
+	
     // Red springs
     glColor3f(1, 0, 0);
     for (unsigned i = 0; i < SPRING_COUNT; i++)
     {
-        const Vector3 *p = springs[i].getAnchor();
-        glVertex3f(p->x, p->y, p->z);
+		//const Vector3 p = particleArray[i+2].getPosition();
+        //glVertex3f(p.x, p.y, p.z);
+
+		glVertex3f(particleArray[2+i].getPosition().x, particleArray[2+i].getPosition().y, particleArray[2+i].getPosition().z);
+        glVertex3f(particleArray[6+i].getPosition().x, particleArray[6+i].getPosition().y, particleArray[6+i].getPosition().z);
+
     }
-	*/
+	
  
     glEnd();
 }
@@ -219,16 +249,27 @@ void Mobile::display()
 // Handles key input
 void Mobile::key(unsigned char key)
 {
+	Vector3 pos2;
 	switch(key)
     {
     case 'w': case 'W':
-        
+		particleArray[1].setPosition(Vector3(0,5,0));
+
+		for(unsigned i = 2; i<6; i++){
+			Vector3 pos = particleArray[i].getPosition();
+			pos.y = pos.y + 2.0;
+			particleArray[i].setPosition(pos);
+		}
+
         break;
     case 'a': case 'A':
         
         break;
     case 'd': case 'D':
-        
+        pos2 = particleArray[4].getPosition();
+		pos2.x = pos2.x + 1.0;
+		particleArray[4].setPosition(pos2);
+
         break;
 
     default:
