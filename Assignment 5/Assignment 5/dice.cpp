f93f52a04c3d5dee7e906b5587e30392f98caef8
carmonj001 <harryperch@hotmail.com>
2014-03-26 23:39:06 +0100

    Añadido custom collider

--- Assignment 5/Assignment 5/dice.cpp
+++ Assignment 5/Assignment 5/dice.cpp
@@ -139,6 +139,9 @@ class DiceDemo : public RigidBodyApplication
     /** Processes the objects in the simulation forward in time. */
     virtual void updateObjects(cyclone::real duration);
 
+	/* Generates the contacts of the dice with a plane. */
+	unsigned diceAndHalfSpace(const Dice &dice, const cyclone::CollisionPlane &plane, cyclone::CollisionData *data);
+
 public:
     /** Creates a new demo object. */
     DiceDemo();
@@ -214,7 +217,8 @@ void DiceDemo::generateContacts()
 	// Check collisions
     for (unsigned i = 0; i < DICE; i++) {
         if(!cData.hasMoreContacts()) return;
-        cyclone::CollisionDetector::boxAndHalfSpace(dices[i], plane, &cData);
+        //cyclone::CollisionDetector::boxAndHalfSpace(dices[i], plane, &cData);
+		diceAndHalfSpace(dices[i], plane, &cData);
         for (int j = i - 1; j >= 0; j--) {
             cyclone::CollisionDetector::boxAndBox(dices[i], dices[j], &cData);
         }
@@ -315,4 +319,70 @@ void DiceDemo::key(unsigned char key)
 Application* getApplication()
 {
     return new DiceDemo();
+}
+
+
+unsigned DiceDemo::diceAndHalfSpace(const Dice &dice, const cyclone::CollisionPlane &plane, cyclone::CollisionData *data)
+{
+    // Make sure we have contacts
+    if (data->contactsLeft <= 0) return 0;
+
+    // Check for intersection
+    if (!cyclone::IntersectionTests::boxAndHalfSpace(dice, plane))
+    {
+        return 0;
+    }
+
+    // We have an intersection, so find the intersection points. We can make
+    // do with only checking vertices. If the box is resting on a plane
+    // or on an edge, it will be reported as four or two contact points.
+
+    // Go through each combination of + and - for each half-size
+    static cyclone::real mults[24][3] = {{0.6,1,1}, {1,0.6,1}, {1,1,0.6},
+										 {-0.6,1,1}, {-1,0.6,1}, {-1,1,0.6},
+										 {0.6,-1,1}, {1,-0.6,1}, {1,-1,0.6},
+										 {-0.6,-1,1}, {-1,-0.6,1}, {-1,-1,0.6},
+										 {0.6,1,-1}, {1,0.6,-1}, {1,1,-0.6},
+										 {-0.6,1,-1}, {-1,0.6,-1}, {-1,1,-0.6},
+	                                     {0.6,-1,-1}, {1,-0.6,-1}, {1,-1,-0.6},
+	                                     {-0.6,-1,-1}, {-1,-0.6,-1}, {-1,-1,-0.6}};
+
+    cyclone::Contact* contact = data->contacts;
+    unsigned contactsUsed = 0;
+    for (unsigned i = 0; i < 24; i++) {
+
+        // Calculate the position of each vertex
+        cyclone::Vector3 vertexPos(mults[i][0], mults[i][1], mults[i][2]);
+        vertexPos.componentProductUpdate(dice.halfSize);
+        vertexPos = dice.getTransform().transform(vertexPos);
+
+        // Calculate the distance from the plane
+        cyclone::real vertexDistance = vertexPos * plane.direction;
+
+        // Compare this to the plane's distance
+        if (vertexDistance <= plane.offset)
+        {
+            // Create the contact data.
+
+            // The contact point is halfway between the vertex and the
+            // plane - we multiply the direction by half the separation
+            // distance and add the vertex location.
+            contact->contactPoint = plane.direction;
+            contact->contactPoint *= (vertexDistance-plane.offset);
+            contact->contactPoint += vertexPos;
+            contact->contactNormal = plane.direction;
+            contact->penetration = plane.offset - vertexDistance;
+
+            // Write the appropriate data
+            contact->setBodyData(dice.body, NULL, data->friction, data->restitution);
+
+            // Move onto the next contact
+            contact++;
+            contactsUsed++;
+            if (contactsUsed == (unsigned)data->contactsLeft) return contactsUsed;
+        }
+    }
+
+    data->addContacts(contactsUsed);
+    return contactsUsed;
 }
